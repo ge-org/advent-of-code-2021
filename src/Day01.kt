492dc4fddd0f90b6f335048311d5d3fc8a97435d
Georg Dresler <georg@g-dresler.de>
2021-12-03 19:26:50 +0100

    Add function to read input file as integers

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,17 @@
 fun main() {
-    fun part1(input: List<String>): Int = input.map(String::toInt).zipWithNext().count { (a, b) -> a < b }
+    fun part1(input: List<Int>): Int = input.zipWithNext().count { (a, b) -> a < b }
 
-    fun part2(input: List<String>): Int = input.map(String::toInt)
+    fun part2(input: List<Int>): Int = input
         .windowed(3)
         .map(List<Int>::sum)
         .zipWithNext().count { (a, b) -> a < b }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInputInt("Day01_test")
     check(part1(testInput) == 7)
     check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInputInt("Day01")
     println(part1(input))
     println(part2(input))
 }
